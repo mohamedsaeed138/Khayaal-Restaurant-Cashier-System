cc85a47f197fa49b09a7f298d06d8f89cf2ee05f
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2022-12-11 00:24:04 +0200

    yyy

---
+++ Guna_UI_Framework_Ultimate_v2.0.0.1/Guna_UI_Framework_Ultimate_v2.0.0.1/Guna_UI_Framework_Ultimate_v2.0.0.1/Guna.UI-Framework-Lib-master/Guna UI Activation/Guna UI Activation/Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Licensing1 = New Guna.UI2.Licensing.Licensing()
+        Me.SuspendLayout()
+        '
+        'Licensing1
+        '
+        Me.Licensing1.BackColor = System.Drawing.Color.White
+        Me.Licensing1.Location = New System.Drawing.Point(0, 0)
+        Me.Licensing1.MaximumSize = New System.Drawing.Size(348, 485)
+        Me.Licensing1.MinimumSize = New System.Drawing.Size(348, 485)
+        Me.Licensing1.Name = "Licensing1"
+        Me.Licensing1.Size = New System.Drawing.Size(348, 485)
+        Me.Licensing1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(348, 485)
+        Me.Controls.Add(Me.Licensing1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Guna UI WinForms"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Licensing1 As Guna.UI2.Licensing.Licensing
+End Class
---
+++ Guna_UI_Framework_Ultimate_v2.0.0.1/Guna_UI_Framework_Ultimate_v2.0.0.1/Guna_UI_Framework_Ultimate_v2.0.0.1/Guna.UI-Framework-Lib-master/README.md
@@ -0,0 +1,38 @@
+# Framework for C#, VB.NET WinForms 
+
+# Guna.UI2 WinForms
+Version: 2.0.0.1
+Update: April 23 2020
+Controls & Components: 53
+
+# Guna.UI WinForms
+Version: 1.9.0.1
+Update: March 23 2020
+Controls & Components: 60
+
+📌 Website
+Link ►  https://gunaframework.com
+
+📌 How to update the Guna.UI
+Link ►  https://youtu.be/N5I-LcdVv34
+
+Guna.UI controls are DLL driven tools to help you build awesome desktop application interfaces. It guarantees great user experience in your apps and reduces development time. The DLLs are installed in your .NET environment and is built on top of WinForms. It enables developers to drag and drop hence faster coding
+# Guna.UI2 v2.0.0.1- Bug Fixed [1] 
+* [bug-fixed] Guna2TextBox (PlaceholderText, MouseDoubleClick Event, MouseClick Event, DoubleClick Event)
+
+# Guna.UI v1.9.0.1 - Bug Fixed [4] 
+* [bug-fixed] GunaTextBox
+* [bug-fixed] GunaLineTextBox
+* [bug-fixed] GunaControlBox
+* [bug-fixed] GunaDateTimePicker
+
+# Guna.UI v1.9.0.1 - License control updated
+* Activate Guna.UI.dll
+* Request 1 month code evaluation (Trial)
+* Deactivate the license used on your computer.
+
+# Follow
+* Youtube ► http://bit.ly/30wfEr0
+* Facebook ► https://facebook.com/sobatdata
+* Instagram ► https://instagram.com/sobatdata
+* Github ► https://github.com/sobatdata
