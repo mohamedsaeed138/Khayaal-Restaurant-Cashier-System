caaf922c2f6d3612134e4e95bf363e4f8a72b197
MeloR1 <ahmedgharieb668@gmail.com>
2022-12-11 03:43:08 +0200

    Added SQL String con

--- Khayaal_SAHM/Main_Form.cs
+++ Khayaal_SAHM/Main_Form.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Windows.Forms;
+using Connection_Strings;
 
 namespace SAHM_Version_ALPHA_0._1
 {
@@ -8,6 +10,7 @@ namespace SAHM_Version_ALPHA_0._1
         public Main_Form()
         {
             InitializeComponent();
+            
         }
 
         private void bunifuSwitch1_Click(object sender, EventArgs e)
