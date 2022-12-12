f6d745001bbb71c9d562fdef3674878ba5d97eab
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2022-12-12 05:17:17 +0200

    .

--- Khayaal_SAHM/Main_Form and Sub_Forms/Main_Form.Designer.cs
+++ Khayaal_SAHM/Main_Form and Sub_Forms/Main_Form.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Main_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Main_Form";
             this.Text = "Main_Form";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
