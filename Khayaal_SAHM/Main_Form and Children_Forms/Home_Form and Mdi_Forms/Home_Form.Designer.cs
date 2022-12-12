3338ab75935df291fddb2725d142f794a72b3194
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2022-12-12 10:13:20 +0200

    Added Folders

--- Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Home_Form.Designer.cs
+++ Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Home_Form.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace Khayaal_SAHM.Main_Form_and_Sub_Forms.Home_Form
+﻿namespace Khayaal_SAHM.Main_Form_and_Children_Forms.Home_Form_and_Mdi_Forms
 {
-    partial class Item
+    partial class Home_Form
     {
-        /// <summary> 
+        /// <summary>
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary> 
+        /// <summary>
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,33 +20,20 @@
             base.Dispose(disposing);
         }
 
-        #region Component Designer generated code
+        #region Windows Form Designer generated code
 
-        /// <summary> 
-        /// Required method for Designer support - do not modify 
+        /// <summary>
+        /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
-            this.SuspendLayout();
-            // 
-            // guna2Elipse1
-            // 
-            this.guna2Elipse1.TargetControl = this;
-            // 
-            // Item
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "Item";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Home_Form";
         }
 
         #endregion
-
-        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
     }
 }
---
+++ Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Home_Form.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+
+namespace Khayaal_SAHM.Main_Form_and_Children_Forms.Home_Form_and_Mdi_Forms
+{
+    public partial class Home_Form : Form
+    {
+        public Home_Form()
+        {
+            Initialize();
+        }
+        public void Initialize()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
