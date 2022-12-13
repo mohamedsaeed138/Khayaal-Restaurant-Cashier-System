ad92e5f59d1b9762f7bc034e4dfc6f71fe01b583
amlrashwan <115794969+amlrashwan20@users.noreply.github.com>
2022-12-13 12:03:45 +0200

    AML DB

--- Khayaal_SAHM/Connection_String.cs
+++ Khayaal_SAHM/Connection_String.cs
@@ -7,9 +7,9 @@
     /// </summary>
     public class Connection_String
     {
-        public const string Value = AG;
+        public const string Value = AR;
         private const string HS = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\Documents\GitHub\SAHM\Khayaal_SAHM\DataBase\Restaurant_Cafe.mdf;Integrated Security=True;Connect Timeout=30";
-        private const string AR = @"";
+        private const string AR = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\SAHM\Khayaal_SAHM\DataBase\Restaurant_Cafe.mdf;Integrated Security=True;Connect Timeout=30";
         private const string KS = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\SAHM\Khayaal_SAHM\DataBase\Restaurant_Cafe.mdf;Integrated Security=True;Connect Timeout=30";
         private const string MS = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\SAHM\Khayaal_SAHM\DataBase\Restaurant_Cafe.mdf;Integrated Security=True";
         private const string AG = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=""E:\SOM DH\SAHM\Khayaal_SAHM\DataBase\Restaurant_Cafe.mdf"";Integrated Security=True";
