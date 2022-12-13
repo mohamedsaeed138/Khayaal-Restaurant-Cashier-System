c055dd834928783aa2417a2d3521774568384c07
MeloR1 <ahmedgharieb668@gmail.com>
2022-12-13 22:28:57 +0200

    .

--- Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Home_Form.Designer.cs
+++ Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Home_Form.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // guna2Panel1
@@ -38,23 +38,23 @@
             this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
-            this.guna2Panel1.Size = new System.Drawing.Size(974, 70);
+            this.guna2Panel1.Size = new System.Drawing.Size(1065, 43);
             this.guna2Panel1.TabIndex = 0;
             // 
-            // panel1
+            // flowLayoutPanel1
             // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 70);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(974, 100);
-            this.panel1.TabIndex = 1;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 43);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1065, 561);
+            this.flowLayoutPanel1.TabIndex = 1;
             // 
             // Home_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(974, 581);
-            this.Controls.Add(this.panel1);
+            this.ClientSize = new System.Drawing.Size(1065, 604);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.guna2Panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Home_Form";
@@ -66,6 +66,6 @@
         #endregion
 
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
