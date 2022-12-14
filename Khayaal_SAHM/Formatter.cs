40ffe8e9b7698327e4571474661d0d07ae485ae8
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2022-12-14 14:44:52 +0200

    .

--- Khayaal_SAHM/Formatter.cs
+++ Khayaal_SAHM/Formatter.cs
@@ -1,21 +1,44 @@
 ﻿using System;
+using System.Data;
 
 namespace Khayaal_SAHM
 {
     public class Formatter
     {
-        public static string String(string String)//not_activated
+        /// <summary>
+        /// to Put any text in Foramt "Mmmmmmm Ssssssss" and erased the first and last space if was existed
+        /// </summary>
+        /// <example> "  MMsassa ssaddedAS" => "Mmsassa Ssaddedas"</example>
+        /// <example> "  MMsassa ssaddedAS" => "Mmsassa Ssaddedas"</example>
+        /// <param name="String"></param>
+        /// <returns></returns>
+        public static string String(string String)
         {
-            if (String == null)
-                String = string.Empty;
-            else
-                String = String;
+
+            string[] Strings = String.Trim().Split(' ');
+            for (int i = 0; i < Strings.Length; i++)
+            {
+                Strings[i] = char.ToUpper(Strings[i][0]) + Strings[i].ToLower().Remove(0, 1);
+
+            }
+
+            String = string.Join(" ", Strings);
+
             return String;
         }
+
+
+
+        /// <summary>
+        /// بيقرب اي سترينج من تنسيق الفلوت لأقرب رقمين عشريين
+        /// </summary>
+        /// <example>زي مثلا تيكست بوكس بياخد سعر او كمية فلازم يتقرب قبل ما تتبعت للداتا بيز</example>
+        /// <param name="String">Float string Need TO BE ROUNDED </param>
+        /// <returns>Sring</returns>
         public static string Float(string String)//not_Avtivated
         {
 
-            return String;
+            return Math.Round(float.Parse(String), 2).ToString();
         }
         /// <summary>
         /// Correct The Date_Time_Picker Value Formating
@@ -43,10 +66,31 @@ namespace Khayaal_SAHM
         /// <param name="To">The End of Range</param>
         /// <returns>Boolean</returns>
         public static bool Check_Payment_Date_Range(DateTime From, DateTime To) => DateTime.Parse(Date_Formating(From, "From_Payment")) < DateTime.Parse(Date_Formating(To, "To_Payment"));//activated
+        /// <summary>
+        /// Check there is no any intersections With Other Bookings Date Ranges
+        /// </summary>
+        /// <param name="Bookings">List of Booking of the same table</param>
+        /// <returns>bool</returns>
 
+        [Obsolete("Under_Development", true)]
+        public bool Check_Bookings_Conflicts(DataTable Bookings)//not activated
+        {
 
+            foreach (DataRow row in Bookings.Rows)
+            {
+                if (true)
+                {
+                    return false;
 
+                }
+            }
+            return true;
+        }
 
 
     }
+
+
+
 }
+
