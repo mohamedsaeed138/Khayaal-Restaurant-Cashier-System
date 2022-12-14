041c01da30317ad1063573b8a74618500bb487c4
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2022-12-14 07:46:15 +0200

    .

---
+++ Khayaal_SAHM/Formatter.cs
@@ -0,0 +1,31 @@
+﻿namespace Khayaal_SAHM
+{
+    internal class Formatter
+    {
+        public static string String(string String)
+        {
+            if (String == null)
+                String = string.Empty;
+            else
+                String = String;
+            return String;
+        }
+        public static string Float(string String)
+        {
+            if (String == null)
+                String = string.Empty;
+            else
+                String = String;
+            return String;
+        }
+        public static string Date(string String)
+        {
+            if (String == null)
+                String = string.Empty;
+            else
+                String = String;
+            return String;
+        }
+
+    }
+}
