a1b00ea91057333cb21f9de7100eaddb7eeb4a83
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2022-12-23 06:39:06 +0200

    .

---
+++ Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Order_Query.sql
@@ -0,0 +1,16 @@
+﻿BEGIN TRANSACTION Decrease_Quantites
+ 
+  EXEC CR.Decrease_Raw_Materials_Qty	
+	@Item_Id=1,@Qty=5;
+ 
+ IF (Select COUNT(CR.Raw_Materials.Id) FROM CR.Raw_Materials WHERE Qty<0)>0
+    BEGIN 
+      ROLLBACK TRANSACTION Decrease_Quantites
+      
+    END
+  ELSE
+    BEGIN
+      COMMIT TRANSACTION Decrease_Quantites
+	  
+    END
+	SELECT * from cr.Raw_Materials;
