d6f1bdd41dc6d3c6221e8cf24f42099ccfda1b2c
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2022-12-25 17:05:57 +0200

    .

--- Khayaal_SAHM/Report_Path.cs
+++ Khayaal_SAHM/Report_Path.cs
@@ -1,4 +1,6 @@
-﻿namespace Khayaal_SAHM
+﻿using System;
+
+namespace Khayaal_SAHM
 {
     //USE it FOR SQL CONNNECTION OBJECT
     /// <summary>
@@ -7,12 +9,8 @@
     /// </summary>
     public class Report_Path
     {
-        public const string Value = MS;
-        private const string HS = @"C:\Users\User\Documents\GitHub\SAHM\Khayaal_SAHM\Main_Form and Children_Forms\Bills_Form and Mdi_Forms\Print _Form\Print_Report.rdlc";
-        private const string AR = @"";
-        private const string KS = @"";
-        private const string MS = @"E:\SAHM\Khayaal_SAHM\Main_Form and Children_Forms\Bills_Form and Mdi_Forms\Print _Form\Print_Report.rdlc";
-        private const string AG = @"E:\SOM DH\SAHM\Khayaal_SAHM\Main_Form and Children_Forms\Bills_Form and Mdi_Forms\Print _Form\Print_Report.rdlc";
+        public static string Value = $"{Environment.CurrentDirectory}\\Main_Form and Children_Forms\\Bills_Form and Mdi_Forms\\Print _Form\\Print_Report.rdlc";
+
     }
 
 }
