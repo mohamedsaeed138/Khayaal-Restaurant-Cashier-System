05bcec6236f62ecbbebf318d037810c745d266a0
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2023-01-14 21:38:24 +0200

    ز

--- Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Home_Form.cs
+++ Khayaal_SAHM/Main_Form and Children_Forms/Home_Form and Mdi_Forms/Home_Form.cs
@@ -344,12 +344,15 @@ namespace Khayaal_SAHM.Main_Form_and_Children_Forms.Home_Form_and_Mdi_Forms
                 MessageBox.Show(Warning_Message + "\n To Create The Order");
 
                 //Start of Removable Area
+
                 DialogResult r = System.Windows.Forms.MessageBox.Show("Do you want to add though ?", "Warning", MessageBoxButtons.YesNo);
                 if (DialogResult.Yes == r)
                 {
                     return true;
                 }
+
                 //End of Removable Area
+
                 return false;//never remove this line !!
 
             }
