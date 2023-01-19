a707620b97e996fe053e8ca46d4d6b36ffcc174f
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2023-01-19 13:49:28 +0200

    ,

--- Khayaal_SAHM/Main_Form and Children_Forms/Purchases_Form and Mdi_Forms/Add_PS_Mdi_Form.cs
+++ Khayaal_SAHM/Main_Form and Children_Forms/Purchases_Form and Mdi_Forms/Add_PS_Mdi_Form.cs
@@ -49,10 +49,11 @@ namespace Khayaal_SAHM.Main_Form_and_Children_Forms.Purchases_Form_and_Mdi_Forms
         private void Unite_Price_Text_Box_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((!char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != 8)
-            || (e.KeyChar == '.' && Unit_Price_Text_Box.Text.Contains(".")))
+            || (e.KeyChar == '.' && Unit_Price_Text_Box.Text.Contains(".")) || (Unit_Price_Text_Box.Text.Length >= 9 && e.KeyChar != 8))
             {
                 e.Handled = true;
             }
+
         }
 
 
@@ -134,10 +135,11 @@ namespace Khayaal_SAHM.Main_Form_and_Children_Forms.Purchases_Form_and_Mdi_Forms
         private void Qty_Text_Box_KeyPress(object sender, KeyPressEventArgs e)
         {
             if ((!char.IsDigit(e.KeyChar) && e.KeyChar != '.' && e.KeyChar != 8)
-            || (e.KeyChar == '.' && Qty_Text_Box.Text.Contains(".")))
+            || (e.KeyChar == '.' && Qty_Text_Box.Text.Contains(".")) || (Qty_Text_Box.Text.Length >= 9 && e.KeyChar != 8))
             {
                 e.Handled = true;
             }
+
         }
 
 
