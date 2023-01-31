70a979e58378a9c0c554fae91365499b661ebd2b
mohamedsaeed138 <91873184+mohamedsaeed138@users.noreply.github.com>
2023-01-31 02:53:40 +0200

    .

--- Khayaal_SAHM/Properties/AssemblyInfo.cs
+++ Khayaal_SAHM/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SAHM Version ALPHA 0.1")]
+[assembly: AssemblyTitle("Khayaal_SAHM 1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SAHM Version ALPHA 0.1")]
+[assembly: AssemblyProduct("Khayaal_SAHM 1")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
